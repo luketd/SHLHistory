dc108a2120ff8f3e013dfdbeb030570e0bdf210c
luketd <lukedamato99@gmail.com>
2022-06-22 17:22:24 -0400

    Added additional function

--- R/shlFunctions.R
+++ R/shlFunctions.R
@@ -86,3 +86,41 @@ draftResult <- function(today){
 
 
 }
+
+#' Looks into the DB for what trades happened that day
+#'
+#' @param today todays date
+#' @export
+#'
+tradeResultAll <- function(today){
+  ### Todays Date
+  tradeQuery <- paste0("SELECT * FROM TradesAll WHERE strftime('%m-%d',date)=  '",today,"'")
+  getTradeData <- dbGetQuery(con, tradeQuery )
+  max<-nrow(getTradeData)
+  print(max)
+  if(max == 0){
+    return()
+  } else {
+    for (i in 1:max){
+      print(i)
+      if(is_empty(getTradeData$Team3Recieves[i]) == TRUE){
+        tradeResult <- paste0("On ", getTradeData$Date[i], "(", getTradeData$Season[i] ,") \n", getTradeData$Team1[i], "(",
+                              getTradeData$GM1[i],") Recieves: ", getTradeData$Team1Recieves[i], "\n",
+                              getTradeData$Team2[i], "(", getTradeData$GM2[i],") Recieves: ", getTradeData$Team2Recieves[i], "\n and",
+                              getTradeData$Team3[i], "(", getTradeData$GM3[i],") Recieves: ", getTradeData$Team3Recieves[i],
+                              "<",getTradeData$Link[i], ">"
+        )
+
+
+      } else {
+        tradeResult <- paste0("On ", getTradeData$Date[i], "(", getTradeData$Season[i] ,") \n", getTradeData$Team1[i], "(",
+                              getTradeData$GM1[i],") Recieves: ", getTradeData$Team1Recieves[i], "\n",
+                              getTradeData$Team2[i], "(", getTradeData$GM2[i],") Recieves: ", getTradeData$Team2Recieves[i], "\n",
+                              "<",getTradeData$Link[i], ">"
+        )
+      }
+      send_webhook_message(tradeResult)
+      print(tradeResult)
+    }
+  }
+}
